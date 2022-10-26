fa85a7521f39eb29191e06ca6c302d7b6a208a6a
Malla Amruta <61353921+Amruta190@users.noreply.github.com>
2022-10-26 18:15:48 +0530

    Index page added

--- Login&Registration/logout.php
+++ Login&Registration/logout.php
@@ -2,5 +2,5 @@
    session_start();
    unset($_SESSION["username"]);
    echo 'You have successfully Logged Out!';
-   header('Refresh: 1; URL = ../Homepage.html');
+   header('Refresh: 1; URL = ../index.html');
 ?>
