d0fbd5324690cc7bd2ed8594a17d7989cc3cc01c
Malla Amruta <61353921+Amruta190@users.noreply.github.com>
2022-10-26 21:58:26 +0530

    Update Registration.php

--- Login&Registration/Registration.php
+++ Login&Registration/Registration.php
@@ -2,7 +2,7 @@
 require '..\vendor\autoload.php';
 // S3 Handshake
 
-$credentials = new Aws\Credentials\Credentials('AKIASU53LD6JEZQLWFFQ', 'pD1G9bjNpTSRPltIxdTej+zlEv4mjqHX1e+gI4cc');
+$credentials = new Aws\Credentials\Credentials('', '');
 					$s3 = new Aws\S3\S3Client([//create s3 cient
 					    'version'     => 'latest',
 					    'region'      => 'ap-south-1',
