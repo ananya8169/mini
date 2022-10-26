cc1f5be7538aedbf3209df06a8b2be9ef597df72
Malla Amruta <61353921+Amruta190@users.noreply.github.com>
2022-10-26 21:59:27 +0530

    Update user.php

--- Login&Registration/user.php
+++ Login&Registration/user.php
@@ -13,7 +13,7 @@ include('functions.php');
 //S3 Handshake
 
 
-$credentials = new Aws\Credentials\Credentials('AKIASU53LD6JEZQLWFFQ','pD1G9bjNpTSRPltIxdTej+zlEv4mjqHX1e+gI4cc');
+$credentials = new Aws\Credentials\Credentials('','');
 
 $s3 = new Aws\S3\S3Client([
 	'version'     => 'latest',
